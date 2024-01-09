d206efdaefb317a361d53b8c96add856b3b52458
rekurrenzk <Fatihburak@protonmail.com>
2024-01-09 19:23:37 +0300

    bro learning

--- bro/str_slicing.py
+++ bro/str_slicing.py
@@ -1,1 +1,29 @@
 
+"""
+slicing is creating a substring by extracting elem from another string
+"""
+name = 'rekurrenzk'
+first_name = name[3]
+
+print(first_name)
+print(name[3])
+print(name[5])
+print(name[1:4])
+print(name[0:4:2])
+print(name[:-3])
+print(name[-1])
+
+# [start:stop:step]
+
+funky = name[::]
+print(funky)
+reverse_funky = name[::-1]
+print(reverse_funky)
+
+website_url = 'http://www.rekurrenzk.com'
+slice = slice(7,-1)
+
+print(website_url[slice])
+
+
+
