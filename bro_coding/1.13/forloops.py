0647d017acede446eb815bba93ccb6be88fb9aca
rekurrenzk <Fatihburak@protonmail.com>
2024-01-09 20:29:26 +0300

    bro learning

---
+++ bro_coding/1.13/forloops.py
@@ -0,0 +1,30 @@
+
+
+# for loops are block of codes in a limited of time
+
+#while loops are uunlimited
+#for loops are limited
+
+import time as t
+
+for i in range(10, 20+1,3):
+
+    print(i, end="")
+
+    print()
+
+for j in 'rekurrenzk':
+    print(j, ' ', end='')
+print()
+
+print()
+for second in range(10, 0, -1):
+    print(second)
+    t.sleep(1)
+
+
+p = print
+
+p('happy birthday')
+
+
