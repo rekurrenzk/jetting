b6404d3c44fea55f18ba56b697866eec487d40df
rekurrenzk <Fatihburak@protonmail.com>
2024-01-09 19:06:08 +0300

    bro learning

---
+++ bro/mathf.py
@@ -0,0 +1,24 @@
+
+"""
+
+the link for the course : https://www.youtube.com/watch?v=XKHEtdqhLK8
+
+"""
+
+import math
+
+
+pi = 3.1415
+negative_pi = pi*-1
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(abs(negative_pi))
+
+print(math.sqrt(pi))
+print(math.pow(pi,3))
+print(math.exp(pi))
+print(max(1,2,3,4,5,6))
+print(min(1,2,3,4,5,6))
+
